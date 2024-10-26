a667fed82eec5960b1d86980a85d099ca767fae0
nassim.berkhli <nassim.berkhli@epita.fr>
2024-10-26 14:52:52 +0200

    tmp

--- language/src/quiz_phrase.py
+++ language/src/quiz_phrase.py
@@ -1,50 +1,29 @@
 import random
-
-from data.phrases import phrases
+from backend.wordreference import get_examples
+from src.user_input_functions import get_user_input
 from src.print_functions import print_correctly
-from src.user_input_functions import get_user_input, continue_to_ask_user_input
 
-"""
-    expression en anglais,
-    liste des traductions possibles de l'expression en francais
-"""
-
-def quiz_phrase(expression, list_expressions) :
-
+def quiz_phrase(expression, translations):
     user_input = ""
 
-    if not(expression in phrases) :
-        return
+    for key, sub_dict in translations.items() :
 
-    list_phrases = {key: phrases[expression][key] for key in list_expressions
-        if key in phrases[expression]}
+        if len(sub_dict["examples"]) > 1 :
 
-    list_phrases = list(list_phrases.keys())
-    phrases_about_expression = phrases[expression]
+            translate = sub_dict["examples"][1]
 
-    while list_phrases and user_input != 'q':
+            print("Traduisez :")
+            user_input = get_user_input("Votre réponse", example_fr)
+            if user_input == 'q':
+                break
 
-        expression_fr = random.choice(list_phrases)
-        print("Traduisez :")
-        user_input = get_user_input("Votre réponse",
-                                    phrases_about_expression[expression_fr]["question"])
-        if user_input == 'q' :
-            break
-
-        elif user_input != 'n' :
-
-            correct_answer = phrases_about_expression[expression_fr]["answer"]
             result = "[INCORRECTE]"
-            if user_input == correct_answer :
+            if user_input == correct_answer:
                 result = "[CORRECTE]"
-                list_phrases.remove(expression)
+                examples.remove((example_fr, correct_answer))
 
-            print(f"\n{result}\n\n\n")
+            print(f"\n{result}\n")
+            if result == "[INCORRECTE]":
+                input(print_correctly(f"Réponse : '{correct_answer}'"))
 
-            if result == "[INCORRECTE]" :
-                input(print_correctly(f"Réponse : '{correct_answer}' :"))
-
-        else :
-            list_phrases.remove(expression)
-
-        print("\n-----------------------------------------")
+            print("\n-----------------------------------------")
